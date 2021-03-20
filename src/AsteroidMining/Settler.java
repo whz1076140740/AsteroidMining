67227cc18833db6d26af459d83d9fd8f87e03a8c
whz1076140740 <whz1076140740@gmail.com>
2021-03-20 23:43:34 +0800

    Settler sunstorm CanDie 接口
    更改了Settler 更改了Sunstorm
    Settler 还需要添加部分函数 和添加asteroid函数
    SunStorm 还需要检查

--- src/AsteroidMining/Settler.java
+++ src/AsteroidMining/Settler.java
@@ -1,23 +1,60 @@
 package AsteroidMining;
 
-import java.util.ArrayList;
+import java.io.BufferedReader;
 
-public class Settler implements Die{
+public class Settler extends Worker implements CanDie{
 
-    public void Move(){}
+    Field f = new Field();
+    Asteroid a = new Asteroid();
 
-    public void Addresource(Resources r, Aseteroid a){}
+    //Mine
+    public void Mine(Asteroid a){
+        //remove resource
+        a.Removeresource();
+        String enter;
+        System.out.println("Whether the explosion conditions are met at this time?(Yes or No)");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        enter = br.readLine();
+        if(enter.equals("Yes"))
+        {
+            Testing.methodStart("Explode(settler1)");
+        }
+        System.out.println("Whether the sun storm conditions are met at this time?(Yes or No)");
+        BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));
+        enter = br1.readLine();
+        if(enter.equals("Yes"))
+        {
+            Testing.methodStart("Touch(settler1)");
+        }
+        Testing.methodStart(" Settler.Mine()");
+    }
 
-    public ArrayList<String> Getresource(){};
+    public void Addresource(Resources r, Aseteroid a){
+        Testing.methodStart(" Settler.Addresource()");
+    }
 
-    public void Removeresources(Resources r){}
+    public ArrayList<String> Getresource(){
+        Testing.methodStart(" Settler.Getresource()");
+    }
 
-    public void Hide();
+    public void Removeresources(Resources r){
+        Testing.methodStart(" Settler.Hide()");
+    }
 
-    public void Constructrobot(f:Field){}
+    public void Hide(){
+        Testing.methodStart(" Settler.Removeresources()");
+    }
 
-    public void Constructgate(f:Field){}
+    public void Constructrobot(Field f){
+        Testing.methodStart(" Settler.Constructrobot()");
+    }
 
-    public void Die(f:Field){}
+    public void Constructgate(Field f){
+        Testing.methodStart(" Settler.Constructgate()");
+    }
+
+    public void Die(Field f){
+        Testing.methodStart(" Settler.Die()");
+    }
 
 }
