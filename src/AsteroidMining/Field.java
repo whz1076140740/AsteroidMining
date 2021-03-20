e86a9aee9107ca93355c688fb0c43d42542e8c24
KuMua0122 <haruna572168248@gmail.com>
2021-03-20 14:34:01 +0100

    Asteroid+Robot改动初稿

--- src/AsteroidMining/Field.java
+++ src/AsteroidMining/Field.java
@@ -4,4 +4,7 @@ import java.util.ArrayList;
 
 public class Field {
    public ArrayList<Field> field = new ArrayList<Field>();
+
+   public void Accept(Thing t){}
+   public void Remove(Thing t){}
 }
--- src/AsteroidMining/Robot.java
+++ src/AsteroidMining/Robot.java
@@ -2,17 +2,15 @@ package AsteroidMining;
 
 public class Robot extends Worker{
      public Robot() {}
-     public void Hide(Asteroid a) {
-    	 if(a.Getdepth()==0 && a.GetResource()=="hollow") {
-    		 
-    	 }
+     public void Hide() {
+    	  Testing.methodStart("The robot is hiding");
      }
      public void HitbyExplode() {
     	 Travel();
     	 System.out.println("Explosion happens to the robot but fortunately no damage to it");
      }
      public void Die(Field f) {
-    	 f.Remove();
+    	 f.Remove(this);
     	 System.out.println("The robot is destroyed");
      }
 }
