6d1ad3edfc958df00c8f97f3258fd045ee5a6885
whz1076140740 <whz1076140740@gmail.com>
2021-03-20 19:52:23 +0800

    Update Settler.java
    Implement Settler class  method

---
+++ src/AsteroidMining/Settler.java
@@ -0,0 +1,21 @@
+package AsteroidMining;
+
+public class Settler implements Die{
+
+    public void Move(){}
+
+    public void Addresource(Resources r, Aseteroid a){}
+
+    public ArrayList<String> Getresource(){};
+
+    public void Removeresources(Resources r){}
+
+    public void Hide()
+
+    public void Constructrobot(f:Field){}
+
+    public void Constructgate(f:Field){}
+
+    public void Die(f:Field){}
+
+}
