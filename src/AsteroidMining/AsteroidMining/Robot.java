ce313fdc44446c0680c120d2ee3fc1a41f42147b
LensentDsz <72199427+LensentDsz@users.noreply.github.com>
2021-04-19 17:58:18 +0800

    Update Robot.java

--- src/AsteroidMining/AsteroidMining/Robot.java
+++ src/AsteroidMining/AsteroidMining/Robot.java
@@ -10,29 +10,6 @@ public class Robot extends Worker{
        Testing.methodStart("Robot.Tp()");
        Testing.methodEnd("Robot.Tp()");
      }
-     public void Drill(Asteroid asteroid) throws IOException 
-    {
-	// reduce the depth of asteroid and check that if ra and fulfills condition then explode
-	    if(asteroid.depth!= 0){
-			asteroid.depth -= 1;
-			if(asteroid.depth ==0&&asteroid.resource.equals("uranium")&& asteroid.distancetosun< 20){
-               this.HitbyExplode();
-			   Remove(asteroid); //fulfills the explode condition , worker dies
-			}                    //and this asteroid is removed from the field
-		}
-		else{
-			System.out.println("The asteroid is already drilled through!");
-		}
-		asteroid.Move();  //asteroid and sun storm moves after worker does the action
-		st.move();
-		//after sun storm moves, if sun storm touches worker then worker dies.
-		for(int i = 0; i < st.alist.size; i++){
-			if(st.alist.get(i).id == asteroid.id){
-				st.Touch(this);
-			}
-		}
-  }
-     //Test case Robot Hide
      
      //Test case Robot Hide
      public void Hide() {
